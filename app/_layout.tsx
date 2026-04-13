c6e650eb684f7fe3b3c6c4767114e57a977deb0d
ValdemirJunior2020 <your-infojr.83@gmail.com>
2026-04-13 10:41:14 -0400

    Test AuthProvider only

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,16 +4,16 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ToastProvider } from '@/context/ToastContext';
+import { AuthProvider } from '@/context/AuthContext';
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <ToastProvider>
+        <AuthProvider>
           <StatusBar style="light" />
           <Stack screenOptions={{ headerShown: false }} />
-        </ToastProvider>
+        </AuthProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
